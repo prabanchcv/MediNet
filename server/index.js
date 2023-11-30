cd3569d5d4c253b56f95c60c0e62abfe0e5a1b44
prabanch <prabanchcv@gmail.com>
2023-12-01 00:17:13 +0530

    deploy7

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const { Server } = require("socket.io");
 const socketManager = require("./config/socket");
 const mongoSanitize= require("express-mongo-sanitize")
 const xss = require("xss-clean")
-
+const path = require('path');
 
 app.use(cors);
 
@@ -24,6 +24,11 @@ app.use("/images", express.static("images"));
 app.use("/", userRoute);
 app.use("/admin", adminRoute);
 app.use("/doctor", doctorRoute);
+// Serve static files from the React build output directory
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 const server = app.listen(3000, () => {
   console.log("connected");
